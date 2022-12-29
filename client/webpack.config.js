2c2f4b4abaa96300de2ee4447e304d7f47f2dc27
Xiaojing <8redfishcreative@gmail.com>
2022-12-29 10:12:51 -0500

    update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+     //Creates a manifest.json file
+      new WebpackPwaManifest({
+         fingerprints: false,
+         inject: true,
+         name: 'Text Editor',
+         short_name:'Editor',
+         description: 'Write your text in the editor',
+         start_url:'./',
+         publicPath: './',
+         icons:[
+           {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+           },
+         ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //We use babel-loader in order to use ES6
+          use:{
+             loader: 'babel-loader',
+             options:{
+                 presets: ['@babel/preset-env'],
+                 plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],  
+             },
+          },
+        },
       ],
     },
   };
