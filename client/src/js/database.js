9fb7f7cfc43ece9b5a6a8c25f127db5b3f204dfd
Xiaojing <8redfishcreative@gmail.com>
2023-01-06 11:44:00 -0500

    refactor database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
       const jateDb = await openDB('jate', 1);
       const tx = jateDb.transaction('jate', 'readwrite');
       const store = tx.objectStore('jate');
-      const request = store.put({value: content});
+      const request = store.put({jate: content});
       const result = await request;
       console.log('Data saved to the database', result);
        };
